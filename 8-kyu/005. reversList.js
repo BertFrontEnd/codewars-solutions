62377ea10d5f84ee3752446148a2b94b24babf25
BertFrontEnd <Berton@yandex.ru>
2023-03-11 16:33:02 +0300

    feat: add solutions of 8-kyu

--- 8-kyu/005. reversList.js
+++ 8-kyu/005. reversList.js
@@ -1,3 +1,9 @@
+/*
+Description:
+Write a function that reverses a list, without using any built-in reverse functions.
+JS : Array#reverse has been modified.
+*/
+
 const reverseList = (arr) => arr.reverse();
 
 console.log(reverseList([]), '<=> []');
